8f30bca26de252795ac8ea05ae4e80883e11e27c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-01 11:45:04 -0300

    aula 4

---
+++ aula-4_1.3.24/filmes.js
@@ -0,0 +1,47 @@
+const listaFilmes = document.querySelector('#listaFilmes')
+
+const filme_input = document.querySelector('#filmeInput').value
+const btn = document.querySelector('button')
+
+// const filmes = ['as branquelas', 'simpson: o filme', 'persejackson', 'avatar', 'jumper']
+const filmes = [{
+    nome: 'Harry Potter',
+    lancamento: 2001,
+    genero: 'fantasia',
+    atores: {
+        ator1: 'daniel radcliff'
+    }
+},{
+    nome: 'gente grande',
+    lancamento: 2004,
+    genero: 'comedia',
+}]
+
+
+// filmes.push('gente grande') // adiciona algo na lista
+// filmes.pop() // remove o ultomo elemento da lista
+
+// btn.addEventListener('click', ()=>{
+//     let filme_digitado = filme_input.value
+//     filmes.push(filme_digitado)
+// })
+
+
+// window.onload = function carregar_filmes (){}
+window.onload = () => { 
+    for(let i=0; i< filmes.length; i++){
+        let lista = document.createElement('li') // cria um elemento
+        listaFilmes.append(lista)
+        lista.innerHTML = filmes[i].nome
+
+
+        // let listaAno = document.createElement('li') // cria um elemento
+        // listaFilmes.append(listaAno) // adiciona as coisas em uma lista ja criada
+        // listaAno.innerHTML = filmes[i].lancamento // pega os dados especificos
+
+        let listaAno = document.createElement('li') // cria um elemento
+        lista.append(listaAno) // adiciona as coisas em uma lista ja criada
+        listaAno.innerHTML = `o ano de lancameno é: ${filmes[i].lancamento}` // pega os dados especificos
+
+    }
+} // fucao igual a anterior, so que menor
---
+++ aula-4_1.3.24/js.js
@@ -0,0 +1,21 @@
+// const numeros = ['ola', 2,true,{},'herege'] // array - uma lista
+
+// // console.log(numeros)
+// // console.log(numeros[0])
+// // console.log(numeros[4])
+
+// // const [saudacao, num, bool, obj, ideologia] = numeros
+// // const nume = numeros[1]
+
+// // console.log(saudacao)
+// // console.log(ideologia)
+// // console.log(nume)
+
+// // const [saudacao, ... resto] = numeros
+
+// // console.log(saudacao)
+// // console.log(resto)
+
+// numeros[4] = 'carol'
+
+// console.log(numeros.length)
