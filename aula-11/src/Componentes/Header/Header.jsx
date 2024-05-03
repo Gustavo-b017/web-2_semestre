4706f8fdf0fba1db1a89013c141027f6eefe16e9
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-03 11:54:23 -0300

    cine app

---
+++ aula-11/src/Componentes/Header/Header.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../NavBar/NavBar";
+
+function Header() {
+    return ( 
+        <>
+            <span>Ola, bixinho</span>
+            <h1>Bem-vindo ao MovieApp</h1>
+            <NavBar/>
+        </>
+    );
+}
+
+export default Header;
---
+++ aula-11/src/Componentes/NavBar/NavBar.jsx
@@ -0,0 +1,7 @@
+function NavBar() {
+    return ( 
+        <h1>bar</h1>
+    );
+}
+
+export default NavBar;
