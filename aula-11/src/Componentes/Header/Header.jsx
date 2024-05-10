25e1b4a843429b6f2bed3a49d3a60ea58b105632
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-10 10:25:29 -0300

    oi

--- aula-11/src/Componentes/Header/Header.jsx
+++ aula-11/src/Componentes/Header/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from "../NavBar/NavBar";
 function Header() {
     return ( 
         <>
-            <span>Ola, bixinho</span>
+            <span>Ola, cara</span>
             <h1>Bem-vindo ao MovieApp</h1>
             <NavBar/>
         </>
